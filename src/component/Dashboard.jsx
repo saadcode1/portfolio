5a5553ea9ce2e49b931309fc8efcb253a80d0bf5
saad nezami <mdsaadnezami@gmail.com>
2024-08-20 17:19:39 +0530

    structure is complised

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -3,26 +3,54 @@ import "./Dashboard.css"
 export default function Dashboard(){
     return(
         <div className="profile">
-        <div className="first-div">
+         <div className="first-container">
+         <div className="first-div">
          first div
         </div>
-         <div className="product-div">
+
+        <div className="articals">
+            works and features
+         </div>
+         </div>
+        <div className="sec-container">
+         <div className="sec-inner">
+        <div className="product-div">
         product div
          </div>
 
          <div className="third-div">
             profiles
          </div>
-
-         <div className="feature">
+         </div>
+         <div className="service">
+            service offering
+         </div>
+        </div>
+      
+    <div className="third-container">
+    <div className="feature">
             works and features
          </div>
-     
+         <div className="projects">
+            projects div
+         </div>
+
+         <div className="letest">   
+            letest-workss
+         </div>
+    </div>
         
-         <div className="articals">
-            works and features
+        
+        <div className="last-container">
+        <div className="skills">
+            skills
          </div>
 
+         <div className="work">
+            geting touch
+         </div>
+        </div>
+        
         </div>
     )
 }
